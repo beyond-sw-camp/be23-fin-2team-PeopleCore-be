da3529db584493d8dd7179b8f84d396095dc08e2
jmj010702 <bnm07@naver.com>
2026-03-31 14:19:07 +0900

    불필요한 @EnableJpaAuditing 삭제

--- config-server/src/main/java/com/peoplecore/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/peoplecore/config_server/ConfigServerApplication.java
@@ -3,11 +3,9 @@ package com.peoplecore.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableJpaAuditing
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
--- eureka-server/src/main/java/com/peoplecore/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/peoplecore/eureka_server/EurekaServerApplication.java
@@ -3,11 +3,9 @@ package com.peoplecore.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableJpaAuditing
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
