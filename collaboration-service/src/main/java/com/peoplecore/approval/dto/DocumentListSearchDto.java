310c8e84ab45c48e075e80fe37f2a9089914946c
jmj010702 <bnm07@naver.com>
2026-04-05 02:10:12 +0900

    query dsl를 사용하기 위한 빈 생성 query dsl를 사용하기 위해 인터페이스 생성 문서 조회 인터페이스 구현체 생성 문서 조회 서비스 생성 문서 조회 컨트롤러 생성

---
+++ collaboration-service/src/main/java/com/peoplecore/approval/dto/DocumentListSearchDto.java
@@ -0,0 +1,24 @@
+package com.peoplecore.approval.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DocumentListSearchDto {
+    private String search; /*제목, 기안자명, 문서번호 검색*/
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate startDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate endDate;
+    private Long formId;
+    private String status;
+}
