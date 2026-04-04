f88ee1e1ddf3b42cbc7f369e855bab37e8492f40
jmj010702 <bnm07@naver.com>
2026-04-05 02:11:56 +0900

    ApprovalDocumentListController @RequestMapping 주소 변경

--- collaboration-service/src/main/java/com/peoplecore/approval/controller/ApprovalDocumentListController.java
+++ collaboration-service/src/main/java/com/peoplecore/approval/controller/ApprovalDocumentListController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/approval/document")
+@RequestMapping("/approval/documents")
 public class ApprovalDocumentListController {
     private final ApprovalDocumentListService listService;
 
