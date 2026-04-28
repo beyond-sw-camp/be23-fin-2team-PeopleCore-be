491e700521bbfb54f8290623eb13b83b3e0b302d
lampshub <dkdkwlsgml@gmail.com>
2026-04-28 12:43:16 +0900

    급여대장 플로우 정리

--- hr-service/src/main/java/com/peoplecore/pay/approval/PayrollApprovalDocCreatedService.java
+++ hr-service/src/main/java/com/peoplecore/pay/approval/PayrollApprovalDocCreatedService.java
@@ -3,21 +3,29 @@ package com.peoplecore.pay.approval;
 import com.peoplecore.event.PayrollApprovalDocCreatedEvent;
 import com.peoplecore.exception.CustomException;
 import com.peoplecore.exception.ErrorCode;
+import com.peoplecore.pay.domain.PayrollEmpStatus;
 import com.peoplecore.pay.domain.PayrollRuns;
+import com.peoplecore.pay.enums.PayrollEmpStatusType;
+import com.peoplecore.pay.repository.PayrollEmpStatusRepository;
 import com.peoplecore.pay.repository.PayrollRunsRepository;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class PayrollApprovalDocCreatedService {
 
     private final PayrollRunsRepository payrollRunsRepository;
+    private final PayrollEmpStatusRepository payrollEmpStatusRepository;
 
-
-    public PayrollApprovalDocCreatedService(PayrollRunsRepository payrollRunsRepository) {
+    @Autowired
+    public PayrollApprovalDocCreatedService(PayrollRunsRepository payrollRunsRepository, PayrollEmpStatusRepository payrollEmpStatusRepository) {
         this.payrollRunsRepository = payrollRunsRepository;
+        this.payrollEmpStatusRepository = payrollEmpStatusRepository;
     }
 
 
@@ -33,6 +41,14 @@ public class PayrollApprovalDocCreatedService {
         run.submitApproval(event.getApprovalDocId());
         log.info("[PayrollDocCreated] payrollRunId={}, status={}, docId={}",
                 run.getPayrollRunId(), run.getPayrollStatus(), event.getApprovalDocId());
+
+        // CONFIRMED 사원들에게 docId 바인딩
+        List<PayrollEmpStatus> confirmedEmps = payrollEmpStatusRepository
+                .findByPayrollRuns_PayrollRunIdAndStatus(
+                        run.getPayrollRunId(), PayrollEmpStatusType.CONFIRMED);
+        for (PayrollEmpStatus pes : confirmedEmps) {
+            pes.bindApprovalDoc(event.getApprovalDocId());
+        }
     }
 
 }
