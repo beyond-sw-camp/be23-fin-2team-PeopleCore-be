c1f5e3d44c2dadd56414547313eda731fbd67c2c
hwangjuwan <juwan7056@naver.com>
2026-05-08 07:06:08 +0900

    파일함 업로드 시 새로운 url(presigned url)의 CORS 처리

--- collaboration-service/src/main/java/com/peoplecore/common/config/MinioConfig.java
+++ collaboration-service/src/main/java/com/peoplecore/common/config/MinioConfig.java
@@ -9,7 +9,10 @@ import org.springframework.context.annotation.Configuration;
 public class MinioConfig {
 
     @Value("${minio.endpoint}")
-    private String endPoint;
+    private String internalEndpoint;
+
+    @Value("${minio.public-endpoint}")
+    private String publicEndpoint;
 
     @Value("${minio.access-key}")
     private String accessKey;
@@ -17,11 +20,18 @@ public class MinioConfig {
     @Value("${minio.secret-key}")
     private String secretKey;
 
-
-    @Bean
+    @Bean(name = "minioClient")
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint(endPoint)
+                .endpoint(internalEndpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    @Bean(name = "minioPresignClient")
+    public MinioClient minioPresignClient() {
+        return MinioClient.builder()
+                .endpoint(publicEndpoint)
                 .credentials(accessKey, secretKey)
                 .build();
     }
